cbaafefc3adcf020c2171f421b962c347587cef3
unknown <fatihoztemir66@gmail.com>
2020-05-28 19:24:55 +0300

    Bildirim sayfası tasarlandı

--- Mobil/lib/ui/bildirim.dart
+++ Mobil/lib/ui/bildirim.dart
@@ -1,8 +1,7 @@
+import 'package:deneme/ui/ilanVer.dart';
 import 'package:flutter/material.dart';
 
 class Bildirim extends StatefulWidget {
-
-
   @override
   BildirimState createState() {
     return new BildirimState();
@@ -10,7 +9,6 @@ class Bildirim extends StatefulWidget {
 }
 
 class BildirimState extends State<Bildirim> {
-
   @override
   void initState() {
     super.initState();
@@ -18,8 +16,64 @@ class BildirimState extends State<Bildirim> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("BİLDİRİM",style: TextStyle(fontSize: 24),)),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: 40),
+        height: MediaQuery.of(context).size.height,
+        width: double.infinity,
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Text(
+              "BİLDİRİMLER",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 29,
+                  fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: 15,
+                  itemBuilder: (BuildContext ctxt, int index) =>
+                      _anasayfaGridKisi(ctxt, index)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _anasayfaGridKisi(BuildContext ctxt, int index) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+            bottomLeft: Radius.circular(20)),
+      ),
+      elevation: 15.0,
+      child: Container(
+        child: ListTile(
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (BuildContext context) {
+              return IlanVer();
+            }));
+          },
+
+          enabled: true,
+          //////// bildirime sebep olan kişinin fotoğrafı
+          leading: FlutterLogo(),
+          //selected: true,
+          //isThreeLine: true,
+          //title: Text("bildirim ${index}"),
+          subtitle: Text(
+            "1-Fatih Öztemir Size mesaj attı. (Bildirim ${index}). 2- Fatih Öztemir size x puan verdi. " +
+                "3-Fatih Öztemir Sizin Profilinize Baktı(?). 4-Naşka neler olabılır düşün ",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
     );
   }
 }
